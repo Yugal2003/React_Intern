dd2dc3ff917b2cd0442ead9e40c019ecabae25d0
Yugal <yugalramesh6832@gmail.com>
2024-09-04 10:12:00 +0530

    Second Commit

--- src/components/PracticeOnlyUseReducer.js
+++ src/components/PracticeOnlyUseReducer.js
@@ -1,3 +1,5 @@
+//Both code are same just state.value and state changes
+
 // import React, { useReducer } from 'react'
 
 // const reducer = (state,action) =>{
@@ -34,33 +36,107 @@
 
 
 
-import React, { useReducer } from 'react'
+// import React, { useReducer } from 'react'
 
-let initialValue = {
-    value : 0
-};
+// let initialValue = {
+//     value : 0
+// };
 
-const reducer = (state,action) =>{
-    switch (action.type){
-        case "add" :
-        return {...state,value : state.value + 1};
-        case "substract" :
-        return {...state,value : state.value - 1};
-        default : return state;
-    }
-}
-const PracticeOnlyUseReducer = () => {
+// const reducer = (state,action) =>{
+//     switch (action.type){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+//         case "add" :
+//         return {...state,value : state.value + 1};
+//         case "substract" :
+//         return {...state,value : state.value - 1};
+//         default : return state;
+//     }
+// }
+// const PracticeOnlyUseReducer = () => {
 
-    const [state,dispatch] = useReducer(reducer,initialValue);
-  return (
-    <div>
-        <div>
-            <button onClick={()=> dispatch({type : "substract"})}>Minus</button>
-            {state.value}
-            <button onClick={()=> dispatch({type : "add"})}>Plus</button>
-        </div> 
-    </div>
-  )
-}
+//     const [state,dispatch] = useReducer(reducer,initialValue);
+//   return (
+//     <div>
+//         <div>
+//             <button onClick={()=> dispatch({type : "substract"})}>Minus</button>
+//             {state.value}
+//             <button onClick={()=> dispatch({type : "add"})}>Plus</button>
+//         </div> 
+//     </div>
+//   )
+// }
 
-export default PracticeOnlyUseReducer
+// export default PracticeOnlyUseReducer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useReducer } from 'react'
+
+// const reducer = (state,action) =>{
+//     switch(action.type){
+//         case "Add":
+//             return {...state,value:state.value + 1};
+//         case "Substract":
+//             return {...state,value:state.value - 1};
+//         case "Multiply":
+//             return {...state,value:state.value * 2};
+//         case "Division":
+//             return {...state,value:state.value / 2};
+//         case "Module":
+//             return {...state,value:state.value % 2};
+//         default :
+//             return state;
+//     }
+// }
+// let initialValue = {
+//     value : 0
+// }
+
+// const PracticeOnlyUseReducer = () => {
+
+//     const [state,dispatch] = useReducer(reducer,initialValue);
+
+//   return (
+//     <div>
+//         <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",width:"100vw",minHeight:"30vh"}}>
+//             <h1>{state.value}</h1>
+//             <div style={{display:"flex",flexDirection:"row",gap:"1rem",marginTop:"1rem"}}>
+//                 <button onClick={()=> dispatch({type : "Add"})}>Add</button>
+//                 <button onClick={()=> dispatch({type : "Substract"})}>Substract</button>
+//                 <button onClick={()=> dispatch({type : "Multiply"})}>Multiply</button>
+//                 <button onClick={()=> dispatch({type : "Division"})}>Division</button>
+//                 <button onClick={()=> dispatch({type : "Module"})}>Module</button>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default PracticeOnlyUseReducer
